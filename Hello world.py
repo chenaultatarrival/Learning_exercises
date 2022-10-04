fc872beaa285588731336893b38f707e2fbdb2d7
chenault <chenault@arrival.com>
2022-10-04 15:07:36 +0100

    test

--- Hello world.py
+++ Hello world.py
@@ -1,2 +1,16 @@
 # let's learn some stuff!
 print('hello world')
+
+
+numberlist = [1, 2, 3, 7 ,5]
+target = 6
+
+
+def twoSum(nums: numberlist, target: target):
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[j] == target - nums[i]:
+                    return [i, j]
+
+twoSum(numberlist, target)
+
